29a169d5e57ca535f8d901787e4be9f22e3166ee
King <wangyc@retechcorp.com>
2020-07-28 19:27:49 +0800

    select dialog

--- src/main.js
+++ src/main.js
@@ -1,16 +1,31 @@
 const BrowserWindow = require('electron').remote.BrowserWindow
-const settingBtn = document.querySelector('#setting')
-window.onload = function () {
-    settingBtn.onclick = function (e) {
-        if (BrowserWindow.getFocusedWindow().getSize()[1] === 500) {
-            BrowserWindow.getFocusedWindow().setSize(400, 400, false)
-        } else {
-            BrowserWindow.getFocusedWindow().setSize(400, 500, false)
-        }
-    }
+const { dialog } = require('electron').remote
 
+function settingClick(e) {
+    if (BrowserWindow.getFocusedWindow().getSize()[1] === 500) {
+        BrowserWindow.getFocusedWindow().setSize(400, 400, false)
+    } else {
+        BrowserWindow.getFocusedWindow().setSize(400, 500, false)
+    }
 }
 
+function outputChange(event) {
+    debugger
+}
+
+function selectfile() {
+    dialog.showOpenDialog({
+        filters: [
+            { name: 'jpg', extensions: ['jpg'] },
+            { name: 'png', extensions: ['png'] },
+            { name: 'jpg', extensions: ['jpeg'] }
+        ],
+        properties: ['openDirectory'],
+        title: 'select picture',
+    }).then(res => {
+        debugger
+    })
+}
 
 
 
